86b0180b774b769a334aba834486ce226b760d03
StarkTH <su791213t@hotmail.com>
2021-11-30 09:47:48 +0800

    modify main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from app import create_app
 from flask_restful import Api
-from resources.user import User, Users
+from resources.user import UserSignIn, UserSignUp, UserUpdate
 from resources.product import Product, Products
 from resources.addToCar import AddToCar, AddToCars
 
@@ -8,8 +8,9 @@ app = create_app('development')
 
 api = Api(app)
 
-api.add_resource(User, "/user/<int:user_Id>")
-api.add_resource(Users, "/users")
+api.add_resource(UserSignIn, "/users/SignIn")
+api.add_resource(UserSignUp, "/users/SignUp")
+api.add_resource(UserUpdate, "/users/<int:user_Id>")
 
 api.add_resource(Product, "/product/<int:product_Id>")
 api.add_resource(Products, "/products")
